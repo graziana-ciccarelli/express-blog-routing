818157aec7e1633e5c19ad851f11a6857b3c497e
Graziana Ciccarelli <grazianaciccarelli99@gmail.com>
2025-02-13 10:04:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const postsRouter = require('./routers/posts');
 
 app.use(express.json());
 
-// Usa il router dei post
+
 app.use('/api', postsRouter);
 
-// Avvia il server
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
